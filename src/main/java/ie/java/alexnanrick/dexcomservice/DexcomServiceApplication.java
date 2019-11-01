c832735d78c05a27ad6740e1db27b1a21ed317d7
alexnanrick <alex.nanrick@gmail.com>
2019-11-01 20:41:46 +0000

    Add src files

---
+++ src/main/java/ie/java/alexnanrick/dexcomservice/DexcomServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.java.alexnanrick.dexcomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DexcomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DexcomServiceApplication.class, args);
+	}
+
+}
